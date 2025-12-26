70235682c82727c5025422e1d323f34b4e8afd7b
Mkhamat <streetmc1337@gmail.com>
2025-12-26 17:00:28 +0300

    fix

--- sber_pay_android/android/src/main/kotlin/plugin/sdk/SberPayPlugin.kt
+++ sber_pay_android/android/src/main/kotlin/plugin/sdk/SberPayPlugin.kt
@@ -112,7 +112,7 @@ class SberPayPlugin : FlutterPlugin, ActivityAware, SberPayApi {
             SPaySdkApp.getInstance().pay(
                 method = SPayMethod.WithBankInvoiceId,
                 request = SPaymentRequest(
-                    context = currentActivity.application,
+                    context = currentActivity,
                     apiKey = request.apiKey ?: "",
                     merchantLogin = request.merchantLogin ?: "",
                     bankInvoiceId = request.bankInvoiceId,
